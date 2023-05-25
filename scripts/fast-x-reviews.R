c70a73ed77f20a1610316e0b09f45c8ecfc1a9a9
zief0002 <zief0002@umn.edu>
2023-05-25 09:28:13 -0500

    Updated book

--- scripts/fast-x-reviews.R
+++ scripts/fast-x-reviews.R
@@ -2,12 +2,12 @@ library(rvest)
 library(httr)
 library(stringr)
 
-url = "https://www.rottentomatoes.com/m/fast_x/reviews"
+# url = "https://www.rottentomatoes.com/m/fast_x/reviews"
+# 
+# 
+# x = httr::GET(url, add_headers('user-agent' = 'Gov employment data scraper ([[your email]])'))
 
-
-x = httr::GET(url, add_headers('user-agent' = 'Gov employment data scraper ([[your email]])'))
-
-rotten = read_html("~/Desktop/fastx/fastx.html")
+rotten = read_html("~/Desktop/fastx/fastx12.html")
 #rotten2 = read_html("~/Desktop/fastx/fastx2.html")
 
 pub_reviewer = rotten |>
@@ -56,4 +56,24 @@ fresh_rotten = rotten |>
 
 
 # Create data frame
-data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d1 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d2 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d3 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d4 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d5 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d6 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d7 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d8 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d9 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d10 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d11 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+d12 = data.frame(reviewer, publication, review_text, score, fresh_rotten)
+
+
+fastx_reviews = rbind(d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12)
+
+fastx_reviews = fastx_reviews |>
+  select(reviewer, publication, score, fresh_rotten, review_text) |>
+  write_csv("~/Desktop/fastx-reviews.csv")
+
+
